0c683c6c65faddba684d09dfb1924c9fbbb794a0
pratika719 <pratikagrawal719@gmail.com>
2026-05-18 13:37:02 +0530

    added sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,40 @@
-export default function sitemap() {
+import { MetadataRoute } from "next"
+
+export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://hotelchandan.com",
+            url: "https://www.hotelchandan.com",
             lastModified: new Date(),
+            changeFrequency: "weekly",
             priority: 1,
+        },
+
+        {
+            url: "https://www.hotelchandan.com/#rooms",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.9,
+        },
+
+        {
+            url: "https://www.hotelchandan.com/#restaurant",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+
+        {
+            url: "https://www.hotelchandan.com/#gallery",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+
+        {
+            url: "https://www.hotelchandan.com/#contact",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.9,
         },
     ]
 }
